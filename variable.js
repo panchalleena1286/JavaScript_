10f57bf5e422d05e4509a17a7d8b76a7cf2f82bd
panchalleena1286 <panchalleena8839@gmail.com>
2022-05-27 22:34:06 +0530

    Variables

---
+++ variable.js
@@ -0,0 +1,24 @@
+var fullname = "Leena Panchal";
+
+//how to name variable
+
+// fullname
+// full-name
+// full_name
+// FullName
+
+var courseName = "React JS Bootcamp";
+
+var isLoggedIn = false;
+
+var loggedCount = 20;
+
+console.log(loggedCount);
+
+var paymentMode;
+
+
+paymentMode = "Credit Card";
+console.log(paymentMode);
+
+console.log(courseName);
