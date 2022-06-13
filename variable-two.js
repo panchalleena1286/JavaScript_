668cf055c2d64923f8f1c9004258a41c264f9b86
panchalleena1286 <panchalleena8839@gmail.com>
2022-06-13 16:32:22 +0530

    Variables

---
+++ variable-two.js
@@ -0,0 +1,30 @@
+const uid  = "abc123";
+
+var fullName = "Leena Panchal";
+var firstName = "Leena";
+var lastName = "Panchal";
+var country = "India";
+var state = "MP";
+
+var email = "Panchal@l.n";
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 0;
+var isLoggedInFromGoggle = false;
+
+console.log(uid);
+console.log(fullName);
+console.log(email);
+
+console.log(`
+with Unique ID: ${uid}
+User FirstName is : ${firstName}
+and LastName is : ${lastName}
+and his email is: ${email}
+and uses the password: ${password}
+his country name is : ${country}
+and State : ${state}
+his courseCount is :${courseCount}
+and is user logged in from Goggle : ${isLoggedInFromGoggle}
+
+`);
