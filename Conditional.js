10b71255e57309585e433eb0def3ea485a89415c
panchalleena1286 <panchalleena8839@gmail.com>
2022-06-14 16:27:13 +0530

    Create Conditional.js

---
+++ Conditional.js
@@ -0,0 +1,17 @@
+var temperature;
+
+temperature = 39;
+
+// var result = temperature < 20;
+// console.log(result);
+
+if(temperature < 20){
+    console.log("Its very cold outside");
+}
+
+if(temperature < 30){
+    console.log("Its moderate outside");
+}
+else{
+    console.log("Its really hot outside");
+}
