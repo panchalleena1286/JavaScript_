4a8b3e78ecbb465a4a8deb733b7e43b5aa3d3ff0
panchalleena1286 <panchalleena8839@gmail.com>
2022-07-14 16:24:50 +0530

    Create jsContext.js

---
+++ jsContext.js
@@ -0,0 +1,15 @@
+/* // sayHello();
+/
+
+// function sayHello(){
+//     console.log("Hello");
+// }*/
+
+if (2 == 2) {
+    console.log("This is true");
+}
+
+var myName = "Leena";
+if(myName === window.myName){
+    console.log("This is true statement");
+}
