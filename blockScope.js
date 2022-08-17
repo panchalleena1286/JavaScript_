3a01d54a9a44c4aba649c219c3f33df038bb4d4d
panchalleena1286 <panchalleena8839@gmail.com>
2022-08-17 15:00:00 +0530

    Create blockScope.js

---
+++ blockScope.js
@@ -0,0 +1,17 @@
+// {
+//     //compound statement
+//     var a = 10;
+//     console.log(a);
+// }
+
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(a);
+console.log(b);//we cannot get value of b and c ouside of block
+console.log(c);
